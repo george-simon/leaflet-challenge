233c71b5c87b97f114141bc0abbe8daced54e736
george-simon <george-simon@users.noreply.github.com>
2020-10-20 19:32:51 -0700

    map build out

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -4,20 +4,44 @@ var myMap = L.map("map", {
     zoom: 3
   });
   
-  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/streets-v11",
-    accessToken: API_KEY
-  }).addTo(myMap);
-  
-  // Store API query variables
-  var baseURL = "https://data.cityofnewyork.us/resource/fhrw-4uyv.json?";
-  var date = "$where=created_date between'2016-01-01T00:00:00' and '2017-01-01T00:00:00'";
-  var complaint = "&complaint_type=Rodent";
-  var limit = "&$limit=10000";
-  
-  // Assemble API query URL
-  var url = baseURL + date + complaint + limit;
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+tileSize: 512,
+maxZoom: 18,
+zoomOffset: -1,
+id: "mapbox/streets-v11",
+accessToken: API_KEY
+}).addTo(myMap);
+
+// Store API query variables
+var baseURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// Assemble API query URL
+var url = baseURL
+ 
+// Grab the data with d3
+d3.json(url).then(response => {
+    // console.log(response);
+
+    // // Create an array to hold location coordinates
+    var locArray = [];
+
+    // Loop through data
+    for (var i = 0; i < response.length; i++) {
+        var location = response[i].location;
+        console.log(response[i].location);
+        // Set the data location property to a variable
+        
+
+        // Add circles to map
+        L.circle(response[i].location, {
+          fillOpacity: 0.75,
+          color: "white",
+          fillColor: color,
+          // Adjust radius
+          radius: response[i].points * 1500
+        }).bindPopup("<h1>" + response[i].place + "</h1> <hr> <h3>magnitude: " + response[i].mag + "</h3>").addTo(myMap);
+
+    }
+
+});
