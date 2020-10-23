7083c489fb91f2812bf71a58845654a6755a4479
george-simon <george-simon@users.noreply.github.com>
2020-10-22 18:36:19 -0700

    comment out color key

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -36,7 +36,7 @@ d3.json(url).then(data => {
                d > 30  ? '#FD8D3C' :
                d >= 11 ? '#FEB24C' :
                d >= 0  ? '#FED976' :
-                          '#FFEDA0';
+                         '#FFEDA0';
     }
 
     // Loop through data
@@ -58,33 +58,33 @@ d3.json(url).then(data => {
         
     }
     
-    // Set up the legend
-    var legend = L.control({ position: "bottomleft" });
-    legend.onAdd = function() {
-      var div = L.DomUtil.create("div", "info legend");
-      var limits = data.options.limits;
-      var colors = data.options.colors;
-      var labels = [];
+    // // Set up the legend
+    // var legend = L.control({ position: "bottomleft" });
+    // legend.onAdd = function() {
+    //   var div = L.DomUtil.create("div", "info legend");
+    //   var limits = data.options.limits;
+    //   var colors = data.options.colors;
+    //   var labels = [];
 
-      // Add min & max
-      var legendInfo = `<h1>Median Income</h1>
-        <div class="labels">
-            <div class="min"> ${limits[0].toLocaleString(undefined,{style:'currency',currency:'USD',maximumSignificantDigits: 3})} </div>
-            <div class="max"> ${limits[limits.length - 1].toLocaleString(undefined,{style:'currency',currency:'USD',maximumSignificantDigits: 4})} </div>
-        </div>`;
+    //   // Add min & max
+    //   var legendInfo = `<h1>Median Income</h1>
+    //     <div class="labels">
+    //         <div class="min"> ${limits[0].toLocaleString(undefined,{style:'currency',currency:'USD',maximumSignificantDigits: 3})} </div>
+    //         <div class="max"> ${limits[limits.length - 1].toLocaleString(undefined,{style:'currency',currency:'USD',maximumSignificantDigits: 4})} </div>
+    //     </div>`;
 
-      div.innerHTML = legendInfo;
+    //   div.innerHTML = legendInfo;
 
-      limits.forEach(function(limit, index) {
-        labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
-      });
+    //   limits.forEach(function(limit, index) {
+    //     labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+    //   });
 
-      div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-      return div;
-    };
+    //   div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+    //   return div;
+    // };
 
-    // Adding legend to the map
-    legend.addTo(myMap);
+    // // Adding legend to the map
+    // legend.addTo(myMap);
     
     
     
